a614bc1e1838d27c18f0bd6fc43ab347d2b72ae7
ws8637282-art <ws8637282@gmail.com>
2026-09-07 09:19:12 +1300

    Complete C# Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,37 @@
-using System;
+Console.Write("Enter your grade percentage: ");
+string gradeInput = Console.ReadLine();
+int grade = int.Parse(gradeInput);
 
-class Program
+string letter;
+
+if (grade >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+    letter = "A";
+}
+else if (grade >= 80)
+{
+    letter = "B";
+}
+else if (grade >= 70)
+{
+    letter = "C";
+}
+else if (grade >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
+
+Console.WriteLine($"Your grade is: {letter}");
+
+if (grade >= 70)
+{
+    Console.WriteLine("Congratulations! You passed the class.");
+}
+else
+{
+    Console.WriteLine("Keep working hard and try again next time.");
 }
