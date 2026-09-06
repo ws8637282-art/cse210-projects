feeb04d253b83a31b760f8bcc93c4833db5135b2
ws8637282-art <ws8637282@gmail.com>
2026-09-07 09:35:11 +1300

    Complete C# Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+List<int> numbers = new List<int>();
+
+Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+int number = -1;
+
+while (number != 0)
 {
-    static void Main(string[] args)
+    Console.Write("Enter number: ");
+    string input = Console.ReadLine();
+    number = int.Parse(input);
+
+    if (number != 0)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        numbers.Add(number);
     }
 }
+
+int sum = 0;
+
+foreach (int num in numbers)
+{
+    sum += num;
+}
+
+double average = (double)sum / numbers.Count;
+
+int largest = numbers[0];
+
+foreach (int num in numbers)
+{
+    if (num > largest)
+    {
+        largest = num;
+    }
+}
+
+Console.WriteLine($"The sum is: {sum}");
+Console.WriteLine($"The average is: {average}");
+Console.WriteLine($"The largest number is: {largest}");
